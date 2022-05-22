3f1612f0f829010575ee709008353b511d641ea6
gattila89 <gattila89@gmail.com>
2022-05-22 13:24:04 +0200

    Targy osztály tulajdonságainak felvétele

--- JatekProgram/src/Model/Targy.java
+++ JatekProgram/src/Model/Targy.java
@@ -1,5 +1,27 @@
 package Model;
 
 public class Targy {
+    private String _nev;
     
+    private Integer _tomeg;
+    
+    public String getNev()
+    {
+        return _nev;
+    }
+    
+    public void setNev(String nev)
+    {
+        _nev = nev;
+    }
+    
+    public Integer getTomeg()
+    {
+        return _tomeg;
+    }
+    
+    public void setTomeg(Integer tomeg)
+    {
+        _tomeg = tomeg;
+    }
 }
