609b520309106f7bae844c51602f04f89338f4da
gattila89 <gattila89@gmail.com>
2022-05-22 17:12:46 +0200

    Targyteszt ok

--- JatekProgram/test/Model/TargyTest.java
+++ JatekProgram/test/Model/TargyTest.java
@@ -9,22 +9,31 @@ public class TargyTest {
     public TargyTest() {
     }
     
-    //@BeforeAll
-    public static void setUpClass() {
+    @Test
+    public void testTargyTestConstruktor_1()
+    {
+        Targy instance = new Targy();
+        String expResult = "";
+        Integer expTomeg = 0;
+        String result = instance.getNev();
+        Integer result2 = instance.getTomeg();
+        assertEquals(expResult, result);
+        assertEquals(expTomeg, result2);
     }
     
-    //@AfterAll
-    public static void tearDownClass() {
+    @Test
+    public void testTargyTestConstruktor_2()
+    {
+        String expResult = "TesztTargy";
+        Integer expTomeg = 0;
+        
+        Targy instance = new Targy(expResult, expTomeg);
+        String result = instance.getNev();
+        Integer result2 = instance.getTomeg();
+        assertEquals(expResult, result);
+        assertEquals(expTomeg, result2);
     }
     
-    //@BeforeEach
-    public void setUp() {
-    }
-    
-    //@AfterEach
-    public void tearDown() {
-    }
-
     @Test
     public void testGetNev() {
         System.out.println("getNev");
@@ -37,16 +46,17 @@ public class TargyTest {
     @Test
     public void testSetNev() {
         System.out.println("setNev");
-        String nev = "";
+        String nev = "Attila";
         Targy instance = new Targy();
         instance.setNev(nev);
+        assertEquals(nev, instance.getNev());
     }
 
     @Test
     public void testGetTomeg() {
         System.out.println("getTomeg");
         Targy instance = new Targy();
-        Integer expResult = null;
+        Integer expResult = 0;
         Integer result = instance.getTomeg();
         assertEquals(expResult, result);
     }
@@ -54,9 +64,10 @@ public class TargyTest {
     @Test
     public void testSetTomeg() {
         System.out.println("setTomeg");
-        Integer tomeg = null;
+        Integer tomeg = 10;
         Targy instance = new Targy();
         instance.setTomeg(tomeg);
+        assertEquals(tomeg, instance.getTomeg());
     }
     
 }
